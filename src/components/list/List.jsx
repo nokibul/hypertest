bbc2da5bda602a7617ac6d2d2467617c7e7fa72e
unknown <nokibnur@gmail.com>
2023-01-12 22:50:57 +0600

    Added list page

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -5,10 +5,27 @@ const List = () => {
   return (
     <>
       <div className="container">
-        CONTACTS
+        <h1 className='page-title'>Contacts</h1>
         <div className="list-container">
           <ul className='list'>
-            
+            <li className='list-item'>
+              <a href=''>
+                <h1>Nokibul</h1>
+                <h3>01617054351</h3>
+              </a>
+            </li>
+            <li className='list-item'>
+              <a href=''>
+                <h1>Nokibul</h1>
+                <h3>01617054351</h3>
+              </a>
+            </li>
+            <li className='list-item'>
+              <a href=''>
+                <h1>Nokibul</h1>
+                <h3>01617054351</h3>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
